d52707448f5a5af06dae68f4239376bdc3332380
amelamel <amel.krouma@gmail.com>
2014-02-26 09:53:13 +0100

    voila le projet android avec la bd ..il manque 2 classes (Module et module_bdd)

---
+++ cahierDeSon/src/com/example/cahierdeson/Utilisateur.java
@@ -0,0 +1,63 @@
+package com.example.cahierdeson;
+
+public abstract class Utilisateur {
+	private int id_utilisateur;
+	protected String nom;
+	protected String prenom;
+	protected String mot_de_pass; //on modifira le type si le mot de passe doit être chiffré ou crypté
+	
+	public Utilisateur(String nom,String prenom,String pass){
+		this.nom=nom;
+		this.prenom=prenom;
+		this.mot_de_pass=pass;
+	}
+	
+	public Utilisateur(String prenom){
+		this.prenom=prenom;
+	}
+	
+	public Utilisateur(){}
+
+	
+	//a completer
+	
+	/*************************************************getters**************************************************/
+	public int getId(){
+		return this.id_utilisateur;
+		}
+	public String getNom(){
+		return this.nom;
+	}
+	
+	public String getPrenom(){
+		return this.prenom;
+	}
+	
+	//on verra par la suite si on chiffre le mot de passe 
+	public String getMotDePasse(){
+		return this.mot_de_pass;
+		
+	}
+	
+	
+	/***************************************************setters*******************************************************/
+	
+	protected void changeMotDePass(String mp){
+		this.mot_de_pass=mp;
+		
+		//a completer
+	}
+	
+	
+	public void setId(int id){
+		this.id_utilisateur=id;
+	}
+	public void setNom(String nom){
+		this.nom=nom;
+	}
+	
+	public void setPrenom(String prenom){
+		this.prenom=prenom;
+	}
+	
+}
